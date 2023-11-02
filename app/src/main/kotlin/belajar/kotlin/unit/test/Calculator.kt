374aeb656ddff385550e9c28623ab3320f6d2917
DvdArdiansyahhh <124945304+DvdArdiansyahhh@users.noreply.github.com>
2023-11-02 21:47:49 +0700

    Penjelasan

--- app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
+++ app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
@@ -2,6 +2,7 @@ package belajar.kotlin.unit.test
 
 class Calculator {
 
+    // Method untuk menambahkan 2 angka dan mengembalikan hasilnya.
     fun add(first: Int, second: Int): Int {
         return first + second
     }
--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -2,15 +2,18 @@ package belajar.kotlin.unit.test
 
 import org.junit.Test
 
+// Class CalculatorTest untuk testing class Calculator
 class CalculatorTest {
 
     val calculator = Calculator()
 
+    // Untuk testing method add ketika penambahan berhasil.
     @Test
     fun testAddSuccess() {
         val result = calculator.add(10, 10)
     }
 
+    // Testing ke-2 yang mana method yang di testing sama dengan di atas
     @Test
     fun testAddSuccess2() {
         val result = calculator.add(10, 10)
